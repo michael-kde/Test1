dd178cbcc04346abec5244a3e27ef6d424c07b0c
michael-kde <michael.kanonik@googlemail.com>
2024-03-31 02:47:29 +0200

    CSVUtils.java

--- src/com/test1/CSVUtils.java
+++ src/com/test1/CSVUtils.java
@@ -2,13 +2,23 @@ package com.test1;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Calendar;
+import java.util.Date;
 
 public class CSVUtils {
 
     
     private static final String NEW_LINE_SEPARATOR = "\n";
 
-    public static void writeLine(Writer w, String DELIMITER, String ...values)  throws IOException {    	
+    /**
+     *
+     *
+     * @param w
+     * @param DELIMITER
+     * @param values
+     * @throws IOException
+     */
+    public static void writeLine(Writer w, String DELIMITER, String ...values)  throws IOException {
     	StringBuilder sb = new StringBuilder();
         for (String value : values) 
         	sb.append(value).append(DELIMITER);
@@ -17,4 +27,18 @@ public class CSVUtils {
         w.append(sb);
     }
 
+    /**
+     *  add days to the calendar
+     *
+     * @param date
+     * @param days
+     * @return
+     */
+    public static Date changeDate(Date date, int days) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, 1);
+
+        return cal.getTime();
+    }
 }
