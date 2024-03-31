9d722e2c89e46b5fc82a196091507d69d37c3318
michael-kde <michael.kanonik@googlemail.com>
2024-03-31 06:59:38 +0300

    Update Main.java

--- src/com/test1/Main.java
+++ src/com/test1/Main.java
@@ -1,7 +1,6 @@
 package com.test1;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -10,8 +9,6 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 /*
@@ -24,7 +21,7 @@ import java.util.List;
 
 public class Main {
 	private static final List<User> users = new ArrayList<User>();
-	
+
 	private static final String DELIMITER = ";";
 	private static final String ORIG_FILE = "testdata.csv";
 	private static final String NEW_FILE = "testdata.new.csv";
@@ -34,55 +31,41 @@ public class Main {
 	private static String HEADERLINE; 
 	
 	public static void main(String[] args) {
-		
 		getData();
 		processData();
 		writeData();
 	  }
 
 	/**
-	 * reads the csv file
+	 * read users from the csv file
 	 */
-	
 	private static void getData() {
-		BufferedReader reader = null;
-
-		try {
+		try (BufferedReader reader = new BufferedReader(new FileReader(PATH+ORIG_FILE)); ) {
 			String line;
 			int lineNumber=0;
-			reader = new BufferedReader(new FileReader(PATH+ORIG_FILE));
-			
+
 			while ((line = reader.readLine()) != null) {
 				System.out.println("Reading the line:\t" +line);
-				
+
 				if(lineNumber == 0) {
 					HEADERLINE = line;
-	                lineNumber++;
-	                continue;
-	            }
-				getUser(line);
+					lineNumber++;
+					continue;
+				}
+				getUserFromLine(line);
 			}
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			if (reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 	}
 	  
 	  /**
-	   * 	
+	   * get user from line
+	   *
 	   * @param line
 	   */
-	  private static void getUser(String line){
+	  private static void getUserFromLine(String line){
 			String[] fields = line.split("\\s*" + DELIMITER + "\\s*");
 			if (fields.length >=5) {
 				try {
@@ -98,65 +81,41 @@ public class Main {
 	  }
 	  
 	  /**
-	   * 
+	   * update users
 	   */
 	  private static void processData() {
 		  if(! users.isEmpty()) {
-			  for(User u : users){
-				  u.setNumber(u.getNumber()+1);
-				  u.setDate(changeDate(u.getDate()));
-			  }
+			  users.forEach(user -> {
+						  user.setNumber(user.getNumber()+1);
+						  user.setDate(CSVUtils.changeDate(user.getDate(), 1));
+					  }
+			  );
+
 		  } else {
 			  System.out.println("There are no users!");
 		  }
 	  }
 
 	  /**
-	   *  add 1 day to the calendar
-	   * 
-	   * @param date
-	   * @return
-	   */
-	  private static Date changeDate(Date date) {
-	      Calendar cal = Calendar.getInstance();
-	      cal.setTime(date);
-	      cal.add(Calendar.DATE, 1);
-	      
-	      return cal.getTime();	
-	  }
-	
-	  /**
 	   * writes users to the csv file 
 	   */
 	  private static void writeData() {
-			Writer writer = null;
-			try {
-				writer = new FileWriter(PATH+NEW_FILE);
-				CSVUtils.writeLine(writer, DELIMITER, HEADERLINE);
+		  try (	Writer writer  = new FileWriter(PATH+NEW_FILE)) {
+			  	CSVUtils.writeLine(writer, DELIMITER, HEADERLINE);
 				
 				if (! users.isEmpty()) {
 					for(User u : users){
 						System.out.println("User after changes:" + u);
-
-						CSVUtils.writeLine(writer, DELIMITER, u.getName(), u.getCity(), u.getCountry(), 
+						CSVUtils.writeLine(writer, DELIMITER, u.getName(), u.getCity(), u.getCountry(),
 								Integer.toString(u.getNumber()), dateFormat.format(u.getDate()));
 					}
 				} else {
 					System.out.println("There are no users!");
 				}
 
-	            System.out.println("CSV file " + NEW_FILE + " was created successfully !");
-			} catch (IOException e) {
+				System.out.println("CSV file " + NEW_FILE + " was created successfully !");
+		  } catch (IOException e) {
 				e.printStackTrace();
-			} finally {
-				if (writer != null) {
-					try {
-						writer.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
+		  }
 	  }
-
-}	
+}
